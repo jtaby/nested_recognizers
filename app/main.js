0620a4c0a38055a7f6ba007180e6c07dd9a22158
Majd Taby <mtaby@me.com>
2011-07-19 12:38:12 -0700

    Adding view twice so you can pinch and pan while you pinch and pan

--- app/main.js
+++ app/main.js
@@ -8,8 +8,7 @@ spade.require('sproutcore-touch');
 
 var NestedRecognizers = SC.Application.create();
 
-NestedRecognizers.OuterView = SC.ContainerView.extend({
-  elementId: 'containerId',
+NestedRecognizers.PinchPanView = SC.ContainerView.extend({
   scale: 1,
 
   translate: {
@@ -32,37 +31,24 @@ NestedRecognizers.OuterView = SC.ContainerView.extend({
         string += ' scale3d('+this.scale+','+this.scale+',1)';
 
     this.$().css('-webkit-transform',string);
-  },
-  
+  }
+});
+
+NestedRecognizers.OuterView = NestedRecognizers.PinchPanView.extend({
+  classNames: ['containerId'],
   childViews: ['nestedView'],
 
-  nestedView: SC.View.extend({
-    elementId: 'nestedId',
+  nestedView: NestedRecognizers.PinchPanView.extend({
+  classNames: ['nestedId'],
+  })
+});
 
-    scale: 1,
+NestedRecognizers.OuterView2 = NestedRecognizers.PinchPanView.extend({
+  classNames: ['containerId'],
+  childViews: ['nestedView'],
 
-    translate: {
-      x: 0,
-      y: 0
-    },
-
-    pinchChange: function(recognizer, scale) {
-      this.scale = scale;
-      this._applyTransforms();
-    },
-
-    panChange: function(recognizer, translation) {
-      this.translate = translation;
-      this._applyTransforms();
-    },
-
-    _applyTransforms: function() {
-      var string = 'translate3d('+this.translate.x+'px,'+this.translate.y+'px,0)';
-          string += ' scale3d('+this.scale+','+this.scale+',1)';
-
-      this.$().css('-webkit-transform',string);
-    }
-    
+  nestedView: NestedRecognizers.PinchPanView.extend({
+  classNames: ['nestedId'],
   })
 });
 
@@ -70,5 +56,6 @@ $(document).ready(function() {
 
   SC.run(function() {
     NestedRecognizers.OuterView.create().append();
+    NestedRecognizers.OuterView2.create().append();
   });
 });
