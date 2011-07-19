9181b73875b4901eeee8d0dc95f0f7cfd3aebdc0
Majd Taby <mtaby@me.com>
2011-07-19 11:51:00 -0700

    use pinch/pan pairs for both views

--- app/main.js
+++ app/main.js
@@ -1,21 +1,74 @@
+// ==========================================================================
+// Project:   NestedRecognizers
+// Copyright: ©2011 My Company Inc. All rights reserved.
+// ==========================================================================
+
+// TODO: Your app code goes here
 spade.require('sproutcore-touch');
+
 var NestedRecognizers = SC.Application.create();
+
 NestedRecognizers.OuterView = SC.ContainerView.extend({
   elementId: 'containerId',
-  pinchChange: function(recognizer, scale) { 
-    this.$().css('-webkit-transform','scale3d('+scale+','+scale+',1)'); 
+  scale: 1,
+
+  translate: {
+    x: 0,
+    y: 0
   },
 
+  pinchChange: function(recognizer, scale) {
+    this.scale = scale;
+    this._applyTransforms();
+  },
+
+  panChange: function(recognizer, translation) {
+    this.translate = translation;
+    this._applyTransforms();
+  },
+
+  _applyTransforms: function() {
+    var string = 'translate3d('+this.translate.x+'px,'+this.translate.y+'px,0)';
+        string += ' scale3d('+this.scale+','+this.scale+',1)';
+
+    this.$().css('-webkit-transform',string);
+  },
+  
   childViews: ['nestedView'],
+
   nestedView: SC.View.extend({
     elementId: 'nestedId',
-    panChange: function(recognizer, translation) { 
-      this.$().css('-webkit-transform','translate3d('+translate.x+'px,'+translate.y+'px,0)'); 
+
+    scale: 1,
+
+    translate: {
+      x: 0,
+      y: 0
+    },
+
+    pinchChange: function(recognizer, scale) {
+      this.scale = scale;
+      this._applyTransforms();
+    },
+
+    panChange: function(recognizer, translation) {
+      this.translate = translation;
+      this._applyTransforms();
+    },
+
+    _applyTransforms: function() {
+      var string = 'translate3d('+this.translate.x+'px,'+this.translate.y+'px,0)';
+          string += ' scale3d('+this.scale+','+this.scale+',1)';
+
+      this.$().css('-webkit-transform',string);
     }
+    
   })
 });
+
 $(document).ready(function() {
-  SC.run(function() { 
-    NestedRecognizers.OuterView.create().append(); 
+
+  SC.run(function() {
+    NestedRecognizers.OuterView.create().append();
   });
 });
